5cbe1324f63ee19af6825e7eaf0dae0c15770367
MrXiao <flangelySpring@163.com>
2019-03-21 22:37:05 +0800

    添加订单id生成工具类

---
+++ flange-store-entity/src/main/java/com/flange/store/util/IdUtil.java
@@ -0,0 +1,35 @@
+package com.flange.store.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author flangely
+ * @create 2019-03-20
+ * <p>主键自增长工具类
+ * 12:14 AM
+ */
+public class IdUtil {
+
+
+//    public static void main (String[] args){
+//       String s = getOrderId();
+//       System.out.println(s);
+//    }d
+    /**
+     * 生成主键id
+     * @return
+     */
+    public static String getGeneralID(){
+       return System.currentTimeMillis() + "" + (int)((Math.random()*9+1)*1000000);
+    }
+    /**
+     * 生成订单Id
+     */
+    public static String getOrderId(){
+        Date now = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
+        return format.format(now) + (int)((Math.random()*9+1)*100000);
+    }
+
+}
