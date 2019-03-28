0c8a52bfb3912032f023ce68f2492884decd88ab
MrXiao <flangelySpring@163.com>
2019-03-28 20:41:38 +0800

    创建修改商品参数承载类

--- flange-store-console/src/main/java/com/flange/store/console/dto/PmsProductParam.java
+++ flange-store-console/src/main/java/com/flange/store/console/dto/PmsProductParam.java
@@ -18,10 +18,6 @@ import java.util.List;
 public class PmsProductParam extends PmsProduct {
     @ApiModelProperty("商品的sku库存信息")
     private List<PmsSkuStock> skuStockList;
-    @ApiModelProperty("商品参数及自定义规格属性")
-    private List<PmsProductAttributeValue> productAttributeValueList;
-    @ApiModelProperty("专题和商品关系")
-    private List<CmsSubjectProductRelation> subjectProductRelationList;
 
 
     public List<PmsSkuStock> getSkuStockList() {
@@ -30,21 +26,5 @@ public class PmsProductParam extends PmsProduct {
 
     public void setSkuStockList(List<PmsSkuStock> skuStockList) {
         this.skuStockList = skuStockList;
-    }
-
-    public List<PmsProductAttributeValue> getProductAttributeValueList() {
-        return productAttributeValueList;
-    }
-
-    public void setProductAttributeValueList(List<PmsProductAttributeValue> productAttributeValueList) {
-        this.productAttributeValueList = productAttributeValueList;
-    }
-
-    public List<CmsSubjectProductRelation> getSubjectProductRelationList() {
-        return subjectProductRelationList;
-    }
-
-    public void setSubjectProductRelationList(List<CmsSubjectProductRelation> subjectProductRelationList) {
-        this.subjectProductRelationList = subjectProductRelationList;
     }
 }
