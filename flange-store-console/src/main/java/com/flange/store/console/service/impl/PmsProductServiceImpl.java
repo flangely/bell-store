aea643e65042d5b6dd91f21c221f176f4f377784
MrXiao <flangelySpring@163.com>
2019-04-15 17:18:57 +0800

    添加商品列表图片显示信息

--- flange-store-console/src/main/java/com/flange/store/console/service/impl/PmsProductServiceImpl.java
+++ flange-store-console/src/main/java/com/flange/store/console/service/impl/PmsProductServiceImpl.java
@@ -40,6 +40,8 @@ public class PmsProductServiceImpl implements PmsProductService {
         int count;
         PmsProduct product = productParam;
         product.setId(IdUtil.getGeneralID());
+        String pic = productParam.getAlbumPics().split(",")[0];
+        product.setPictureUrl(pic);
         productMapper.insertSelective(product);
         count = 1;
         return count;
@@ -59,6 +61,8 @@ public class PmsProductServiceImpl implements PmsProductService {
         int count;
         PmsProduct product = productParam;
         product.setId(id);
+        String pic = productParam.getAlbumPics().split(",")[0];
+        product.setPictureUrl(pic);
         productMapper.updateByPrimaryKeySelective(product);
         count = 1;
         return count;
